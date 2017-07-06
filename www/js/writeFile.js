219ae4398cbe399392379c451131df9a77c2b1c1
Louis-Ferdinand Goffin <louis.ferdinand.goffin@gmail.com>
2017-07-06 17:14:59 +0200

    Fixing issues

--- www/js/writeFile.js
+++ www/js/writeFile.js
@@ -1,20 +1,3 @@
-function createFile() {
-   var type = window.TEMPORARY;
-   var size = 5*1024*1024;
-   window.requestFileSystem(type, size, successCallback, errorCallback)
-
-   function successCallback(fs) {
-      fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
-         alert('File creation successfull!')
-      }, errorCallback);
-   }
-
-   function errorCallback(error) {
-      alert("ERROR: " + error.code)
-   }
-	
-};
-
 function writeFile() {
    var type = window.TEMPORARY;
    var size = 5*1024*1024;
@@ -41,49 +24,4 @@ function writeFile() {
    function errorCallback(error) {
       alert("ERROR: " + error.code)
    }
-};
-
-function readFile() {
-   var type = window.TEMPORARY;
-   var size = 5*1024*1024;
-   window.requestFileSystem(type, size, successCallback, errorCallback)
-
-   function successCallback(fs) {
-      fs.root.getFile('log.txt', {}, function(fileEntry) {
-
-         fileEntry.file(function(file) {
-            var reader = new FileReader();
-
-            reader.onloadend = function(e) {
-               var txtArea = document.getElementById('textarea');
-               txtArea.value = this.result;
-            };
-            reader.readAsText(file);
-         }, errorCallback);
-      }, errorCallback);
-   }
-
-   function errorCallback(error) {
-      alert("ERROR: " + error.code)
-   }
-};
-
-function removeFile() {
-   var type = window.TEMPORARY;
-   var size = 5*1024*1024;
-   window.requestFileSystem(type, size, successCallback, errorCallback)
-
-   function successCallback(fs) {
-      fs.root.getFile('log.txt', {create: false}, function(fileEntry) {
-
-         fileEntry.remove(function() {
-            alert('File removed.');
-         }, errorCallback);
-      }, errorCallback);
-   }
-
-   function errorCallback(error) {
-      alert("ERROR: " + error.code)
-   }
-};
-
+}
