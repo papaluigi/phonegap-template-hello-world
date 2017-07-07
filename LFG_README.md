2f47f911af5eed14ec6e7771b34070fcddd0b884
Louis-Ferdinand Goffin <louis.ferdinand.goffin@gmail.com>
2017-07-07 10:58:01 +0200

    File transfer handling

---
+++ LFG_README.md
@@ -0,0 +1,3 @@
+#REFERENCES
+File local storage :
+https://www.tutorialspoint.com/cordova/cordova_file_system.htm
--- www/js/filesHandler.js
+++ www/js/filesHandler.js
@@ -87,3 +87,45 @@ function removeFile() {
    }
 };
 
+function uploadFile() {
+   var type = window.TEMPORARY;
+   var size = 5*1024*1024;
+   window.requestFileSystem(type, size, successCallback, errorCallback)
+
+   function successCallback(fs) {
+      fs.root.getFile('log.txt', {}, function(fileEntry) {
+	     // !! Assumes variable fileURL contains a valid URL to a text file on the device, 
+    var fileURL = fileEntry.toURL();
+ 
+    var success = function (r) {
+        console.log("Successful upload...");
+        console.log("Code = " + r.responseCode);
+        // displayFileData(fileEntry.fullPath + " (content uploaded to server)"); 
+    }
+ 
+    var fail = function (error) {
+        alert("An error has occurred: Code = " + error.code);
+    }
+ 
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
+    options.mimeType = "text/plain";
+ 
+    var params = {};
+    params.value1 = "test";
+    params.value2 = "param";
+ 
+    options.params = params;
+ 
+    var ft = new FileTransfer();
+    // SERVER must be a URL that can handle the request, like 
+    // http://some.server.com/upload.php 
+    ft.upload(fileURL, encodeURI(http://posttestserver.com/post.php), success, fail, options);
+	  }, errorCallback);
+   }
+   
+   function errorCallback(error) {
+      alert("ERROR: " + error.code)
+   }
+};
