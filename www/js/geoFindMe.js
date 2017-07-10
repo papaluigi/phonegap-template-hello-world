fadcccdfd31e8f6cab85f2b4cf0b1279c49abec6
Louis-Ferdinand Goffin <louis.ferdinand.goffin@gmail.com>
2017-07-10 15:00:38 +0200

    Bug fixing

--- www/js/geoFindMe.js
+++ www/js/geoFindMe.js
@@ -27,6 +27,7 @@ function geoFindMe() {
 	$.post("https://86.238.111.97/write_file.php",
         {
           name : device.uuid,
+		  //name : "LFG",
           lon: longitude,
           lat: latitude,
           evtid: timestamp
